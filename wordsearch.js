81b4cd74e215b9375406b8880d69dbb2e4439f00
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-11-04 04:13:00 +0000

    fixed wordsearch.js and implemented additional features (check if array null, check words vertically)

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = (matrix) => {
+  const verticalJoin = matrix[0].map(function(outerVal, outerIndex) {
+    return matrix.map(innerVal => innerVal[outerIndex]).join("");
+  });
+  return verticalJoin;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (!letters.length) {
+    return true;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+    else {
+      const verticalJoin = transpose(letters);
+      for (const v of verticalJoin) {
+        if (v.includes(word)) return true;
+      }
+    }
+  }
+  
+  return false;
+};
+
+module.exports = wordSearch;
